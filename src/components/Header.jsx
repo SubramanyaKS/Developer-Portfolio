3d95d190d7f598fac87c959b0cbecd9f3bf3ba7c
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-02 22:46:51 +0530

    updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Container, Row, Col } from "react-bootstrap";
 import image from '../assets/image/program.jpg'
 const Header = () => {
   return (
-    <section className="home" id="home"  style={{backgroundColor:"#0a192f", color:"#fff"}}>
+    <section className="home" id="home"  style={{backgroundColor:"#1f293a", color:"#fff"}}>
         <Container fluid >
           <Row>
             <Col xs={12} md={8} className="home-context">
           <h5 className="mb-2">Hello, Hi am</h5>
-          <h1 className="mb-3">Your Name</h1>
+          <h1 className="mb-3 name">Your Name</h1>
           <h6 className="mb-2">I Am <strong>Developer</strong></h6>
           <button>Get in Touch</button>
           </Col>
