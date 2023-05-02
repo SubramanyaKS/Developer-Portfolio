3a2cb4d1020e4f126a1aed77e14f1e41d4a9622e
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-02 12:30:15 +0530

    updated header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+import image from '../assets/image/program.jpg'
+const Header = () => {
+  return (
+    <section className="home" id="home"  style={{backgroundColor:"#0a192f", color:"#fff"}}>
+        <Container fluid >
+          <Row>
+            <Col xs={12} md={8} className="home-context">
+          <h5 className="mb-2">Hello, Hi am</h5>
+          <h1 className="mb-3">Your Name</h1>
+          <h6 className="mb-2">I Am <strong>Developer</strong></h6>
+          <button>Get in Touch</button>
+          </Col>
+          <Col xs={6} md={4}>
+          <img src={image} alt="Program" />
+          </Col>
+          </Row>
+        </Container>
+    </section>
+  )
+}
+export default Header
