74663b2c53bda5c4e1831cf84f10abb596901aa0
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-02 12:30:05 +0530

    updated header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import Education from './components/Education/Education';
+import Header from './components/Header';
+import NavigationBar from './components/NavigationBar'
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Router>
+     <NavigationBar/>
+     <Routes>
+         <Route exact path="/" element={<Header />} />
+         <Route exact path="/education" element={<Education />}/>
+      </Routes>
+   </Router>
   )
 }
 
