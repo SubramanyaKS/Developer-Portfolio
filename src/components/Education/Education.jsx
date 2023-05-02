9ac1e8ba85186a96ec9cccef1467e1b21f5cfb3e
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-02 12:30:48 +0530

    created education

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Education = () => {
+  return (
+    <section>
+        <Container>
+            <h5>Education</h5>
+            
+        </Container>
+    </section>
+  )
+}
+
+export default Education
