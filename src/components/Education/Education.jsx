6c5d725216a5158a67edec36b1201b0e75e9ca9c
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-06 17:56:09 +0530

    updated education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import EducationCard from './EducationCard'
-import './Education.css'
+import './Education.css';
+
 const Education = () => {
   return (
     <section className="education-content" id="education">
         <Container>
             <h1 className="mb-5 title text-center">Education</h1>
             <Row>
-              <Col>
+              <Col className="card-holder">
                 <EducationCard/>
                 <EducationCard />
                 <EducationCard />
