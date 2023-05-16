cd8b86cf4ad0c33819efa504e7bb93c12069e15e
Subramanya K S <subramanyaks2205@gmail.com>
2023-05-16 22:36:40 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
         {/* change name */}
-        <p className="footer-text">Created by Subbu || 2023 All right reserved</p>
+        <p className="footer-text">Created by Subramanya  K S|| 2023 All right reserved</p>
     </div>
   )
 }
