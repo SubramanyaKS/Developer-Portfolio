063cb8e3d1aa4192ecfa5c8994c43799742646a4
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-08 19:56:25 +0530

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Container, Row, Col } from "react-bootstrap";
-import image from '../assets/image/blue1developer.png';
+import image from '../assets/image/developer_2.png';
 import { Typewriter } from 'react-simple-typewriter'
-import '../App.css'
+import '../App.css';
+import { homedata } from '../data/data';
+
 const Header = () => {
   return (
     <section className="home" id="home"  style={{backgroundColor:"#1f293a", color:"#fff"}}>
         <Container >
           <Row>
-            <Col  xs={8} md={8} className="home-content">
-          <h5 className="mb-2">Hello, Hi am</h5>
-          <h1 className="mb-3 name">Your Name</h1>
+            <Col md={6} sm={4} className="home-content">
+          <h5 className="mb-2">{homedata.intro}</h5>
+          <h1 className="mb-3 name">{homedata.name}</h1>
           <h3 className="mb-4">I Am <span>
             <Typewriter 
-            words={['UI Designer', 'Programmer', 'Mern Stack Developer', 'React Native Developer']}
+            words={homedata.tech}
             loop={5}
             cursor
             cursorStyle='_'
@@ -25,8 +27,8 @@ const Header = () => {
           <button>Get in Touch</button>
           <button className="m-2">Resume</button>
           </Col>
-          <Col className="home-pic" xs={8} md={4}>
-          <img src={image}  alt="Program" />
+          <Col className="home-pic"  md={6} sm={4}>
+          <img src={image} height={300} alt="Program" />
           </Col>
           </Row>
         </Container>
