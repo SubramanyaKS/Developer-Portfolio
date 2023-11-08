887678d0c9349998b4b22f347e9ca7783fee1d09
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-08 19:56:06 +0530

    added project

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./project.css";
+import image from "../../assets/image/project.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLink, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+const ProjectCard = ({data}) => {
+  return (
+    <div className="shadow-lg flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <img src={image} class="card-img-top" alt="..." />
+          <h4 className="card-title text-center">{data.title}</h4>
+          <h6 className="card-subtitle text-center">{data.subtitle}</h6>
+        </div>
+        <div className="flip-card-back">
+          <p className="card-text">
+          {data.description}
+          </p>
+          <div className="social-buttons mt-5">
+                    <a href={data.githubLink}>
+                      {" "}
+                      <button className="neo-button">
+                        <FontAwesomeIcon
+                          icon={faGithub}
+                          style={{ fontSize: "24px" }}
+                        />{" "}
+                      </button>
+                    </a>
+                   {data.websiteLink!=null? <a href={data.websiteLink}>
+                      {" "}
+                      <button className="neo-button">
+                        <FontAwesomeIcon
+                          icon={faExternalLink}
+                          style={{ fontSize: "24px" }}
+                        />{" "}
+                      </button>
+                    </a>:null}
+                    {data.downloadLink!=null? <a href={data.downloadLink}>
+                      {" "}
+                      <button className="neo-button">
+                        <FontAwesomeIcon
+                          icon={faDownload}
+                          style={{ fontSize: "24px" }}
+                        />{" "}
+                      </button>
+                    </a>:null}
+                  </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
