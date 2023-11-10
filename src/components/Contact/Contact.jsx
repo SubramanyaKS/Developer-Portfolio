ac909754fd5bbca1f6725dd071f97efbd65699f9
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-10 19:13:47 +0530

    added contact

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import { faFacebookF,faInstagram,faLinkedinIn, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import React from 'react'
+import './contact.css'
+import CircleButton from '../CircleButton/CircleButton'
+
+const Contact = () => {
+    const handleClick = (url)=>{
+        window.open(url, '_blank');
+    }
+  return (
+    <section className='contact' id='contact'>
+        <h2 className="text-center heading mb-0" style={{"fontFamily": "cursive",}}> &nbsp; Connect me on.</h2>
+        <h5 className='mb-2'>Fell free to connect on these sites.</h5>
+        <div className='icon-button-container'>
+            <CircleButton icon={faFacebookF} onClick={()=>handleClick('https://google.com')} />
+            <CircleButton icon ={faXTwitter} onClick={()=>handleClick('https://google.com')} />
+            <CircleButton icon={faInstagram} onClick={()=>handleClick('https://google.com')} />
+            <CircleButton icon ={faLinkedinIn} onClick={()=>handleClick('https://google.com')} />
+        </div>
+
+    </section>
+  )
+}
+
+export default Contact
