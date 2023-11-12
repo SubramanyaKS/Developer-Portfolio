ca7bb1e18663a8d01dfeb266ae81d1cc72187592
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-12 12:32:37 +0530

    updated experience

--- src/components/Experience/ExperienceCard.jsx
+++ src/components/Experience/ExperienceCard.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
+import './experience.css';
 
-const ExperienceCard = () => {
+const ExperienceCard = ({data}) => {
   return (
-    <Card  className="shadow-lg p-3 rounded mb-5">
-      <Card.Header>Year</Card.Header>
+    <Card style={{width: "25rem", fontFamily: "cursive"}} className="shadow-lg p-3 rounded mb-5">
+      <img src={data.image} className="card-img-top" alt="..." />
       <Card.Body>
-        <Card.Title>Degree</Card.Title>
-        <Card.Text>
-          College/ University
-        </Card.Text>
-        <Card.Text>
-            Grade
+        <Card.Title >{data.title}</Card.Title>
+        <Card.Subtitle>{data.subtitle}</Card.Subtitle>
+        <Card.Text className="cardtext">
+          {data.description}
         </Card.Text>
       </Card.Body>
     </Card>
