2979f84927287679bef2d874bdfe91cd219ed34f
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-14 21:36:29 +0530

    updated footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+        {/* change name */}
+        <p className="footer-text p-2">Designed and built by Subramanya</p>
+    </div>
+  )
+}
+
+export default Footer
