8abebd661794d54910aa17bd3ed550467bad66e4
Subramanya K S <subramanyaks2205@gmail.com>
2023-11-18 10:26:24 +0530

    added experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { experiencedata } from '../../data/data'
+import { Container, Row,Col } from 'react-bootstrap'
+import ExperienceCard from './ExperienceCard'
+import './experience.css';
+import { Slide } from 'react-awesome-reveal';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id="experience" className='experience-content'>
+      <h2 className="text-center heading mb-5" style={{"fontFamily": "cursive",}}> &nbsp; Experience.</h2>
+    <Slide direction="up">
+    <Container>
+      <Row>
+        {experiencedata.map((data)=>(
+          <Col key={data.id}>
+          <ExperienceCard data={data}/>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+    </Slide>
+    </section>
   )
 }
 
