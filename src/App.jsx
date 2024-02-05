0b50521a307af4d4e26845e1bcdf35badbb950b7
Subramanya K S <subramanyaks2205@gmail.com>
2024-02-05 07:03:44 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css';
 import About from './components/About/About';
 import Education from './components/Education/Education';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import NavigationBar from './components/NavigationBar'
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import NavigationBar from './components/Navigation/NavigationBar'
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProjectUI from './components/Projects/ProjectUI';
+import Experience from './components/Experience/Experience';
+import Contact from './components/Contact/Contact';
 
 function App() {
-  return (
-  //   
+  return ( 
   <>
   <NavigationBar/>
   <Header/>
   <About/>
+  <Experience/>
   <Education/>
+  <ProjectUI/>
+  <Contact/>
   <Footer/>
   </>
   )
