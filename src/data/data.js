90e61e3b91ba63813843f69c97d4b4f85fb19504
Subramanya K S <subramanyaks2205@gmail.com>
2024-02-06 07:16:34 +0530

    added dummy data

---
+++ src/data/data.js
@@ -0,0 +1,67 @@
+export const aboutdata ={
+  intro:"Hi i am your name.I am passionate about developing complex applications that solve real-world problems impacting millions of users",
+  description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+  image:"./image/men.jpg",
+}
+export const homedata ={
+  name:"Your Name",
+  intro:"Hello, I am",
+  tech:['UI Designer', 'Programmer', 'Mern Stack Developer', 'React Native Developer'],
+
+}
+export const experiencedata=[
+  {
+    id:1,
+    title: "Web Developer",
+    subtitle: "ABC Innovations Pvt. Ltd.",
+    description: "Worked as a web developer helped me to gain tech skills ",
+    image: "./image/project.jpg",
+    githubLink: "https://github.com/",
+    website:"https://google.com",
+  },
+  {
+    id:2,
+    title: "Software Engineer.",
+    subtitle: "XYZ Company.",
+    description: "My First Internship helped me to gain some knowledge on web development. Worked on the project title 'ABC'.",
+    image: "./image/project.jpg",
+    githubLink: "https://github.com/",
+    website:"https://google.com",
+  }
+]
+export const projectsdata =[
+    {
+      id:1,
+      title: "AXB Application",
+      subtitle: "API Based Application",
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text",
+      image: "./assets/image/project.jpg",
+      githubLink: "https://github.com/",
+      websiteLink:"https://google.com",
+      downloadLink:null,
+      tech:[' HTML ',' CSS ']
+
+    },
+    {
+        id:2,
+        title: "Personal Application",
+        subtitle: "Personal Application",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        image: "../assets/image/project.jpg",
+        githubLink: "https://github.com/",
+        websiteLink:"https://google.com",
+        downloadLink:null,
+        tech:[' HTML ',' CSS ']
+      },
+      {
+        id:3,
+        title: "Personal Application",
+        subtitle: "Personal Application",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        image: "../assets/image/project.jpg",
+        githubLink: "https://github.com/",
+        websiteLink:"https://google.com",
+        downloadLink:null,
+        tech:[' HTML ',' CSS ']
+      }
+]
