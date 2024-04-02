eead677ca5e9a07b29b2a6ae58567e02e2ca8c55
Subramanya K S <subramanyaks2205@gmail.com>
2024-04-02 07:15:42 +0530

    updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { faFacebookF,faInstagram,faLinkedinIn, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { Container, Row, Col } from "react-bootstrap";
-import image from '../../assets/image/developer_3.png';
+import image from '../../assets/image/developer_4.png';
 import { Typewriter } from 'react-simple-typewriter'
 import './Header.css';
 import { homedata } from '../../data/data';
+import CircleButton from '../CircleButton/CircleButton';
 
 const Header = () => {
   return (
@@ -13,7 +15,7 @@ const Header = () => {
             <Col md={6} sm={4} className="home-content">
           <h5 className="mb-2">{homedata.intro}</h5>
           <h1 className="mb-3 name">{homedata.name}</h1>
-          <h3 className="mb-4">I Am <span>
+          <h3 className="mb-4">I Am <span className='text-primary'>
             <Typewriter 
             words={homedata.tech}
             loop={5}
@@ -23,12 +25,17 @@ const Header = () => {
             deleteSpeed={50}
             delaySpeed={1000}/>
           </span></h3>
-          
+          <div className='icons-button-container'>
+            <CircleButton icon={faFacebookF} onClick={()=>handleClick('https://google.com')} />
+            <CircleButton icon ={faXTwitter} onClick={()=>handleClick('https://google.com')} />
+            <CircleButton icon={faInstagram} onClick={()=>handleClick('https://google.com')} />
+            <CircleButton icon ={faLinkedinIn} onClick={()=>handleClick('https://google.com')} />
+        </div>
           <button className='outline-button'>Get in Touch</button>
           <button className="outline-button m-2">Resume</button>
           </Col>
           <Col className="home-pic"  md={6} sm={4}>
-          <img src={image} height={300} alt="Program" />
+          <img src={image} height={100} alt="Program" />
           </Col>
           </Row>
         </Container>
